49048f5d89bcb1622f2bc925835ce72d5c837363
Yurkin <sarmishyur@mail.ru>
2025-07-24 10:46:33 +0300

    Улучшена система учёта серверов: валидация, умолчания, статистика

--- servers.py
+++ servers.py
@@ -1,4 +1,15 @@
+import re
+
 servers = []
+
+
+def is_valid_ip(ip):
+    pattern = r'^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$'
+    match = re.match(pattern, ip)
+    if not match:
+        return False
+    # Проверяем, что каждое число <= 255
+    return all(0 <= int(part) <= 255 for part in match.groups())
 
 
 def add_server():
@@ -6,8 +17,14 @@ def add_server():
     ip = input("IP: ")
     role = input("Роль (web, db, cache): ")
     status = input("Статус (online/offline): ")
-    cpu = int(input("CPU: "))
-    ram = int(input("RAM (ГБ): "))
+    print(f"Оставить по умолчанию? (y/n):")
+    standart = input()
+    if standart == "y":
+        cpu = 8
+        ram = 16
+    else:
+        cpu = int(input("CPU: "))
+        ram = int(input("RAM (ГБ): "))
 
     server = {
         "name": name,
@@ -36,9 +53,12 @@ def list_servers():
 
 def find_server_by_ip(ip):
     for server in servers:
-        if server["ip"] == ip:
-            print(f"Найден сервер: {server['name']} | {server['role']} | {server['status']}")
-            return
+        if is_valid_ip(ip):
+            if server["ip"] == ip:
+                print(f"Найден сервер: {server['name']} | {server['role']} | {server['status']}")
+                return
+        else:
+            print("❌ Неверный формат IP-адреса")
     print("❌ Сервер с таким IP не найден.")
 
 
@@ -51,13 +71,37 @@ def remove_server(name):
     print("❌ Сервер не найден.")
 
 
+def online_servers():
+    found = False
+    print("\nСерверы в статусе 'online':")
+    for server in servers:
+        if server["status"] == "online":
+            print(f"  {server['name']} | {server['ip']} | {server['role']} | {server['cpu']} ядер, {server['ram']} ГБ")
+            found = True
+    if not found:
+        print("  ❌ Нет серверов в статусе 'online'")
+    print()
+
+
+def calculate_servers_and_ram():
+    count = 0
+    total_ram = 0
+    for server in servers:
+        count += 1
+        total_ram += int(server["ram"])
+    print(f"Количество серверов: {count}")
+    print(f"Кол-во RAM: {total_ram}")
+
+
 while True:
     print("\n=== Учёт серверов ===")
     print("1. Добавить сервер")
     print("2. Просмотреть все серверы")
     print("3. Найти сервер по IP")
     print("4. Удалить сервер по имени")
-    print("5. Выход")
+    print("5. Найти серверы online")
+    print("6. Найти кол-во серверов и сумму RAM")
+    print("7. Выход")
 
     choice = input("\nВыберите действие: ")
 
@@ -72,6 +116,10 @@ while True:
         name = input("Введите имя сервера: ")
         remove_server(name)
     elif choice == "5":
+        online_servers()
+    elif choice == "6":
+        calculate_servers_and_ram()
+    elif choice == "7":
         print("👋 Выход из программы.")
         break
     else:
